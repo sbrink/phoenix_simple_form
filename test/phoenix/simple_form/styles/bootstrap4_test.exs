74ffa5c23891dbcc8bad9d995ce60a907778b0c5
Robin Böhm <robin.boehm@workshops.de>
2019-02-06 10:16:26 +0100

    test(simpleform): add tests

---
+++ test/phoenix/simple_form/styles/bootstrap4_test.exs
@@ -0,0 +1,11 @@
+defmodule Phoenix.SimpleForm.Styles.Bootstrap4Test do
+  use ExUnit.Case
+
+  import Phoenix.HTML
+  import Phoenix.SimpleForm.Styles.Bootstrap4
+  alias Phoenix.SimpleForm.FormInput
+  alias Phoenix.SimpleForm.Styles.Bootstrap4
+  alias Phoenix.SimpleForm.Factory
+
+  doctest Phoenix.SimpleForm.Styles.Bootstrap4
+end
